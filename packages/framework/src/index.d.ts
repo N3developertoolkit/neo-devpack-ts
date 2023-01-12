2f287cd4cd562e28d364ba849250fa02064bcea3
Harry Pierson <harrypierson@hotmail.com>
2023-01-12 06:40:07 -0800

    update sample contract

--- packages/framework/src/index.d.ts
+++ packages/framework/src/index.d.ts
@@ -3,10 +3,12 @@ type TypedArrayMutableProperties = 'copyWithin' | 'fill' | 'reverse' | 'set' | '
 export interface ByteString extends Omit<Uint8Array, TypedArrayMutableProperties> { 
     readonly [n: number]: number;
 }
+
 export declare const ByteString: ByteStringConstructor;
 export interface ByteStringConstructor {
     concat(one: ByteString, two: ByteString): ByteString;
     from(arrayLike: ArrayLike<number>): ByteString;
+    isValidAddress(account: ByteString): boolean;
 }
 
 export type StorageValue = boolean | bigint | ByteString;
@@ -24,10 +26,48 @@ export interface StorageConstructor {
     delete(context: StorageContext, key: ByteString): void;
 }
 
-// export type Address = void;
+export type Address = ByteString;
 
 // export interface Address {
 //     [Symbol.iterator](): Iterator<number>;
 //  }
 // export const Address: AddressConstructor;
 // export interface AddressConstructor { }
+
+export declare const Runtime: RuntimeConstructor;
+export interface RuntimeConstructor {
+    checkWitness(account: Address): boolean;
+    notify(eventName: string, ...params: any[]);
+    readonly scriptContainer: Transaction;
+}
+
+export interface Transaction {
+    readonly hash: ByteString;
+    readonly version: number;
+    readonly nonce: number;
+    readonly sender: Address;
+    readonly systemFee: bigint;
+    readonly networkFee: bigint;
+    readonly validUntilBlock: bigint;
+    readonly script: ByteString
+}
+
+export declare const Contract: ContractConstructor;
+
+export interface Contract {
+    readonly id: number;
+    readonly updateCounter: number;
+    readonly hash: Address;
+    readonly nef: ByteString;
+    readonly manifest: string;
+}
+
+export interface ContractConstructor {
+    call(hash: Address, method: string, ...params: any[])
+}
+
+export declare const ContractManagement: ContractManagementConstructor;
+export interface ContractManagementConstructor {
+    update(nefFile: ByteString, manifest: string, data?: any): void;
+    getContract(hash: Address): Contract | undefined;
+}
--- src/contract.ts
+++ src/contract.ts
@@ -1,24 +1,89 @@
-import { Storage, ByteString } from '@neo-project/neo-contract-framework';
+import { ByteString, Storage, Address, Runtime, ContractManagement, Contract, Transaction } from '@neo-project/neo-contract-framework';
 
-/** @safe */
-export function symbol() { return "TOKEN"; }
+// this is a prototype NEP-17 contract written in TypeScript.
 
-/** @safe */
-export function decimals() { return 8; }
-
-export function mint(account: ByteString, amount: bigint): void {
-    if (amount === 0n) return;
-    if (amount < 0n) throw new Error("amount must be greater than zero");
-
-    updateBalance(account, amount);
-    updateTotalSupply(amount);
-}
+const SYMBOL = "APOC";
+const DECIMALS = 8n;
+const INITIAL_SUPPLY = 1_000_000n;
 
 const prefixTotalSupply = 0xA0;
 const prefixBalance = 0xA1;
-// const prefixContractOwner = 0xFF;
+const prefixContractOwner = 0xFF;
 
-function updateBalance(account: ByteString, amount: bigint) {
+/** @safe */
+export function symbol() { return SYMBOL; }
+
+/** @safe */
+export function decimals() { return DECIMALS; }
+
+/** @safe */
+export function totalSupply() { 
+    const key = ByteString.from([prefixTotalSupply]);
+    return Storage.get(Storage.currentContext, key) as bigint;
+}
+
+/** @safe */
+export function balanceOf(account: Address) { 
+    if (!ByteString.isValidAddress(account)) throw new Error();
+    const context = Storage.currentContext;
+    const key = ByteString.concat(ByteString.from([prefixBalance]), account);
+    const value = Storage.get(context, key);
+    return value ? value as bigint : 0n;
+}
+
+export function transfer(from: Address, to: Address, amount: bigint, data: any) {
+    if (!ByteString.isValidAddress(from)) throw new Error();
+    if (!ByteString.isValidAddress(to)) throw new Error();
+    if (amount < 0n) throw new Error("The amount must be a positive number");
+    if (!Runtime.checkWitness(from)) return false;
+    if (amount != 0n) {
+        if (!updateBalance(from, -amount)) return false;
+        updateBalance(to, amount);
+    }
+    postTransfer(from, to, amount, data);
+    return true;
+}
+
+export function mint(account: Address, amount: bigint): void {
+    if (amount === 0n) return;
+    if (amount < 0n) throw new Error("amount must be greater than zero");
+    var owner = getOwner();
+    if (!Runtime.checkWitness(owner)) throw new Error();
+
+    createTokens(account, amount);
+}
+
+export function burn(account: Address, amount: bigint): void {
+    if (amount === 0n) return;
+    if (amount < 0n) throw new Error("amount must be greater than zero");
+    var owner = getOwner();
+    if (!Runtime.checkWitness(owner)) throw new Error();
+
+    if (!updateBalance(account, -amount)) throw new Error();
+    updateTotalSupply(-amount);
+    postTransfer(account, null, amount, null);
+}
+
+function postTransfer(from: Address | null, to: Address | null, amount: bigint, data: any) {
+    Runtime.notify("Transfer", from, to, amount);
+    if (to) {
+        const contract = ContractManagement.getContract(to);
+        if (contract) {
+            Contract.call(to, "onNEP17Payment", from, amount, data);
+        }
+    }
+}
+
+function updateTotalSupply(amount: bigint) {
+    const context = Storage.currentContext;
+    const key = ByteString.from([prefixTotalSupply]);
+    const value = Storage.get(context, key);
+    let totalSupply = value ? value as bigint : 0n;
+    totalSupply += amount;
+    Storage.put(context, key, totalSupply);
+}
+
+function updateBalance(account: Address, amount: bigint) {
     const context = Storage.currentContext;
     const key = ByteString.concat(
         ByteString.from([prefixBalance]),
@@ -35,26 +100,31 @@ function updateBalance(account: ByteString, amount: bigint) {
     return true;
 }
 
-function updateTotalSupply(amount: bigint) {
-    const context = Storage.currentContext;
-    const key = ByteString.from([prefixTotalSupply]);
-    const value = Storage.get(context, key);
-    let totalSupply = value ? value as bigint : 0n;
-    totalSupply += amount;
-    Storage.put(context, key, totalSupply);
+/** @deploy */
+export function deploy(data: any, update: boolean) { 
+    if (update) return;
+
+    const key = ByteString.from([prefixContractOwner]);
+    var sender = (Runtime.scriptContainer as Transaction).sender;
+    Storage.put(Storage.currentContext, key, sender);
+    var amount = INITIAL_SUPPLY * (10n ** DECIMALS);
+    createTokens(sender, amount);
 }
 
-// /** @safe */
-// export function getValue() { 
-//     return neo.Storage.get(neo.Storage.currentContext, neo.ByteString.from([0x00])); 
-// }
+export function update(nefFile: ByteString, manifest: string) {
+    var owner = getOwner();
+    if (!Runtime.checkWitness(owner)) throw new Error();
+    ContractManagement.update(nefFile, manifest);
+}
 
-// export function setValue(value: string) { 
-//     neo.Storage.put(neo.Storage.currentContext, neo.ByteString.from([0x00]), value); 
-// }
+function createTokens(account: Address, amount: bigint) {
+    if (amount < 0n) throw new Error("The amount must be a positive number");
+    updateBalance(account, amount);
+    updateTotalSupply(amount);
+    postTransfer(null, account, amount, null);
+}
 
-// /** @safe */
-// export function helloWorld() { return "Hello, World!"; }
-
-// /** @safe */
-// export function sayHello(name: string) { return "Hello, " + name + "!"; }
+function getOwner(): ByteString {
+    const key = ByteString.from([prefixContractOwner]);
+    return Storage.get(Storage.currentContext, key) as Address;
+}
