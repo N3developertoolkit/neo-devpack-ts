08c46e47a83806b0fb782b5e7cedca3ecd01145e
Harry Pierson <harrypierson@hotmail.com>
2023-02-07 14:30:58 -0800

    log file output

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { join } from "path";
-import { readFile, writeFile } from "fs/promises";
+import path from "path";
 import { ts } from "ts-morph";
 import { compile, createContractProject, saveArtifacts, toDiagnostic } from '../packages/compiler/';
 // import { dumpArtifacts } from "./utils";
 import * as fsp from 'fs/promises';
+import * as fs from 'fs';
 
 function printDiagnostics(diags: ReadonlyArray<ts.Diagnostic>) {
     const formatHost: ts.FormatDiagnosticsHost = {
@@ -18,13 +18,14 @@ function printDiagnostics(diags: ReadonlyArray<ts.Diagnostic>) {
 }
 
 const FILENAME = "contract-test.ts";
-const OUTPUT_DIR = "../express";
+const OUTPUT_DIR = "../express/out";
 
 async function main() {
     const project = await createContractProject();
 
     // load test contract
-    const contractSource = await readFile(join(__dirname, FILENAME), 'utf8');
+    const contractName = path.basename(FILENAME, ".ts");
+    const contractSource = await fsp.readFile(path.join(__dirname, FILENAME), 'utf8');
     project.createSourceFile(FILENAME, contractSource);
     project.resolveSourceFileDependencies();
 
@@ -36,29 +37,33 @@ async function main() {
         printDiagnostics(diagnostics.map(d => d.compilerObject));
     } else {
         try {
-            // const { artifacts, context, diagnostics } = 
             const { diagnostics, nef, manifest, debugInfo } = compile({ project });
 
-            if (diagnostics.length > 0) {
-                printDiagnostics(diagnostics);
-            }
+            if (diagnostics.length > 0) printDiagnostics(diagnostics);
+
+            const outputPath = path.join(__dirname, OUTPUT_DIR);
+            if ((nef || manifest || debugInfo) && !fs.existsSync(outputPath))
+                await fsp.mkdir(outputPath);
 
             if (nef) {
-                const nefPath = join(__dirname, OUTPUT_DIR, "contract.nef");
+                const nefPath = path.join(outputPath, `${contractName}.nef`);
                 const $nef = Buffer.from(nef.serialize(), 'hex');
                 await fsp.writeFile(nefPath, $nef);
+                console.log(nefPath);
             }
 
             if (manifest) {
-                const manifestPath = join(__dirname, OUTPUT_DIR, "contract.manifest.json");
+                const manifestPath = path.join(outputPath, `${contractName}.manifest.json`);
                 const $manifest = JSON.stringify(manifest.toJson(), null, 4);
                 await fsp.writeFile(manifestPath, $manifest);
+                console.log(manifestPath);
             }
 
             if (debugInfo) {
-                const debugInfoPath = join(__dirname, OUTPUT_DIR, "contract.debug.json");
+                const debugInfoPath = path.join(outputPath, `${contractName}.debug.json`);
                 const $debugInfo = JSON.stringify(debugInfo, null, 4);
                 await fsp.writeFile(debugInfoPath, $debugInfo);
+                console.log(debugInfoPath);
             }
         } catch (error) {
             printDiagnostics([toDiagnostic(error)]);
