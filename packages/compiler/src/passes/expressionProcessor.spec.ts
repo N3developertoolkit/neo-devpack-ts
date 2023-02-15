159fe92a78461e044b3f05177569a2f836711c01
Harry Pierson <harrypierson@hotmail.com>
2023-02-15 12:23:49 -0800

    some expression tests

--- packages/compiler/src/passes/expressionProcessor.spec.ts
+++ packages/compiler/src/passes/expressionProcessor.spec.ts
@@ -3,7 +3,7 @@ import 'mocha';
 import * as tsm from "ts-morph";
 import { ReadonlyScope, SymbolDef } from '../scope';
 import { createTestProject } from '../scope.spec'
-import { ParseResult, parseStringLiteral } from './expressionProcessor';
+import { parseArrayLiteral, parseBinaryExpression, ParseResult, parseStringLiteral } from './expressionProcessor';
 import { expect } from 'chai';
 import { Operation } from '../types/Operation';
 
@@ -13,24 +13,81 @@ export function testScope(def: SymbolDef): ReadonlyScope {
     return instance(scope);
 }
 
-function testResult(result: ParseResult): Operation[] {
+function expectOk(result: ParseResult): Operation[] {
     expect(result.isOk()).true;
     return result.unwrap();
 }
 
+class TestScope implements ReadonlyScope {
+
+    private readonly map = new Map<tsm.Symbol, SymbolDef>();
+    readonly parentScope: ReadonlyScope | undefined;
+
+    constructor(parentScope?: ReadonlyScope) {
+        this.parentScope = parentScope;
+    }
+
+    get symbols(): IterableIterator<SymbolDef> {
+        return this.map.values();
+    }
+
+    resolve(symbol?: tsm.Symbol): SymbolDef | undefined {
+        if (symbol) {
+            return this.map.get(symbol) ?? this.parentScope?.resolve(symbol);
+        }
+        return undefined;
+    }
+
+    define(def: SymbolDef) {
+        this.map.set(def.symbol, def);
+    }
+}
+// class FakeScope implements Scope {
+
+//     readonly parentScope = undefined;
+
+//     define<T extends SymbolDef>(factory: T | ((scope: Scope) => T)): T {
+//         return typeof factory === 'function' ? factory(this) : factory;
+//     }
+
+//     get symbols(): IterableIterator<SymbolDef> {
+//         throw new Error('Method not implemented.');
+//     }
+//     resolve(symbol: tsm.Symbol): SymbolDef | undefined {
+//         throw new Error('Method not implemented.');
+//     }
+// }
+
 describe("expression parser", () => {
-    it ("parseStringLiteral", () => {
+    it("parseStringLiteral", () => {
 
         const js = /*javascript*/`function test() { return "Hello World";}`;
-        const {sourceFile} = createTestProject(js)
+        const { sourceFile } = createTestProject(js)
         const node = sourceFile.getFirstDescendantByKindOrThrow(tsm.SyntaxKind.StringLiteral);
-        const result = testResult(parseStringLiteral(node));
-        
-        
+        const result = expectOk(parseStringLiteral(node));
+
+
 
 
     });
-        
+
+    it("parseArrayLiteral", () => {
+        const js = /*javascript*/`function test() { return [1,2,3,4,5]; }`;
+        const { sourceFile } = createTestProject(js)
+        const node = sourceFile.getFirstDescendantByKindOrThrow(tsm.SyntaxKind.ArrayLiteralExpression);
+        const scope = new TestScope();
+        const result = expectOk(parseArrayLiteral(node, scope));
+
+    });
+
+    it("parseBinary", () => {
+        const js = /*javascript*/`function test() { return 2 + 2; }`;
+        const { sourceFile } = createTestProject(js)
+        const node = sourceFile.getFirstDescendantByKindOrThrow(tsm.SyntaxKind.BinaryExpression);
+        const scope = new TestScope();
+        const result = expectOk(parseBinaryExpression(node, scope));
+    });
+
     // const builderMock: MethodBuilder = mock(MethodBuilder);
     // const builder = instance(builderMock);
 
@@ -53,26 +110,26 @@ describe("expression parser", () => {
     //         builder: instance(builder),
     //         scope: null!
     //     };
-    
+
     //     it("null constant", () => {
     //         const def = new ConstantSymbolDef(null!, null!, null);
     //         processSymbolDef(def, options);
     //         verify(builder.pushNull()).once();
     //     });
-    
+
     //     it("boolean constant", () => {
     //         const def = new ConstantSymbolDef(null!, null!, true);
     //         processSymbolDef(def, options);
     //         verify(builder.pushBoolean(true)).once();
     //     });
-    
+
     //     it("bigint constant", () => {
     //         const value = BigInt(42);
     //         const def = new ConstantSymbolDef(null!, null!, value);
     //         processSymbolDef(def, options);
     //         verify(builder.pushInt(value)).once();
     //     });
-    
+
     //     it("data constant", () => {
     //         const value = new Uint8Array([1, 2, 3, 4, 5]);
     //         const def = new ConstantSymbolDef(null!, null!, value);
