a8b41d64411a1603745747e35d60cdaed9ed0f55
Harry Pierson <harrypierson@outlook.com>
2023-03-03 23:11:35 -0800

    dumpContractMethod

--- packages/compiler/src/main.ts
+++ packages/compiler/src/main.ts
@@ -1,8 +1,9 @@
 import * as path from "path";
-import { compile, CompileOptions, createContractProject, hasErrors, toDiagnostic } from './index'
+import { CallOperation, CallTokenOperation, compile, CompileOptions, ContractMethod, ConvertOperation, createContractProject, hasErrors, InitSlotOperation, JumpOffsetOperation, LoadStoreOperation, Location, Operation, PushBoolOperation, PushDataOperation, PushIntOperation, SysCallOperation, toDiagnostic } from './index'
 import * as fsp from 'fs/promises';
 import * as fs from 'fs';
-import { ts } from "ts-morph";
+import { Node, ts } from "ts-morph";
+import { sc } from "@cityofzion/neon-core";
 
 const REPO_ROOT = path.join(__dirname, "../../..");
 const FILENAME = "./sample-contracts/helloworld.ts";
@@ -42,11 +43,15 @@ async function main() {
             const options: Partial<CompileOptions> = contractName.startsWith('nep17')
                 ? { standards: ["NEP-17"] } 
                 : {}
-            const { diagnostics, nef, manifest, debugInfo } = compile(project, contractName, options);
+            const { diagnostics, methods, nef, manifest, debugInfo } = compile(project, contractName, options);
 
             if (diagnostics.length > 0) printDiagnostics(diagnostics);
 
             if (hasErrors(diagnostics)) return;
+
+            for (const m of methods ?? []) {
+                dumpContractMethod(m);
+            }
 
             const outputPath = path.join(REPO_ROOT, OUTPUT_DIR);
             if ((nef || manifest || debugInfo) && !fs.existsSync(outputPath))
@@ -56,14 +61,14 @@ async function main() {
                 const nefPath = path.join(outputPath, `${contractName}.nef`);
                 const $nef = Buffer.from(nef.serialize(), 'hex');
                 await fsp.writeFile(nefPath, $nef);
-                console.log("Wrote: " + nefPath);
+                console.log(green, "Wrote: " + nefPath);
             }
 
             if (manifest) {
                 const manifestPath = path.join(outputPath, `${contractName}.manifest.json`);
                 const $manifest = JSON.stringify(manifest.toJson(), null, 4);
                 await fsp.writeFile(manifestPath, $manifest);
-                console.log("Wrote: " + manifestPath);
+                console.log(green, "Wrote: " + manifestPath);
             }
 
             if (debugInfo) {
@@ -72,7 +77,7 @@ async function main() {
                 jsonDebugInfo["document-root"] = REPO_ROOT;
                 const $debugInfo = JSON.stringify(jsonDebugInfo, null, 4);
                 await fsp.writeFile(debugInfoPath, $debugInfo);
-                console.log("Wrote: " + debugInfoPath);
+                console.log(green, "Wrote: " + debugInfoPath);
             }
         } catch (error) {
             printDiagnostics([toDiagnostic(error)]);
@@ -81,3 +86,111 @@ async function main() {
 }
 
 main();
+
+
+function dumpContractMethod(method: ContractMethod) {
+    console.log(magenta, method.symbol.getName());
+    method.operations.forEach((v, i) => {
+        if (v.location) { console.log(cyan, `  ${dumpLocation(v.location)}`); }
+        console.log(`    ${i}: ${dumpOperation(v, i)}`);
+    })
+}
+function dumpLocation(location: Location) {
+    if (Node.isNode(location)) {
+        return location.print();
+    } else {
+        const src = location.start.getSourceFile().getFullText();
+        const start = location.start.getStart();
+        const end = location.end.getEnd();
+        return src.substring(start, end);
+    }
+}
+
+function dumpOperation(op: Operation, currentIndex: number) {
+    switch (op.kind) {
+        case 'convert': {
+            const { type } = op as ConvertOperation;
+            return `${op.kind} ${sc.StackItemType[type]}`
+        }
+        case 'calltoken': {
+            const { token } = op as CallTokenOperation;
+            return `${op.kind} ${token.hash} ${token.method}`
+        }
+        case 'initslot': {
+            const { locals, params } = op as InitSlotOperation;
+            return `${op.kind} ${locals} locals ${params} params`
+        }
+        case 'call': {
+            const { method } = op as CallOperation;
+            return `${op.kind} ${method.getName()}`
+        }
+        case 'jump':
+        case 'jumpif':
+        case 'jumpifnot':
+        case 'jumpeq':
+        case "jumpne":
+        case "jumpgt":
+        case "jumpge":
+        case "jumplt":
+        case "jumple": {
+            const { offset } = op as JumpOffsetOperation;
+            return `${op.kind} ${offset} (${offset + currentIndex})`
+        }
+        case 'syscall':{
+            const { name } = op as SysCallOperation;
+            return `${op.kind} ${name}`
+        }
+        case 'loadarg':
+        case 'loadlocal':
+        case 'loadstatic':
+        case 'storearg':
+        case 'storelocal':
+        case 'storestatic': {
+            const { index } = op as LoadStoreOperation
+            return `${op.kind} ${index}`
+        }
+        case 'pushbool': {
+            const { value } = op as PushBoolOperation;
+            return `${op.kind} ${value}`;
+        }
+        case 'pushdata': {
+            const { value } = op as PushDataOperation;
+            const buffer = Buffer.from(value);
+            return `${op.kind} 0x${buffer.toString('hex')} "${buffer.toString('utf8')}"`;
+        }
+        case 'pushint': {
+            const { value } = op as PushIntOperation;
+            return `${op.kind} ${value}`
+        }
+        default:
+            return `${op.kind}`
+    }
+}
+
+enum AnsiEscapeSequences {
+    Black = "\u001b[30m",
+    Red = "\u001b[31m",
+    Green = "\u001b[32m",
+    Yellow = "\u001b[33m",
+    Blue = "\u001b[34m",
+    Magenta = "\u001b[35m",
+    Cyan = "\u001b[36m",
+    White = "\u001b[37m",
+    Gray = "\u001b[90m",
+    BrightRed = "\u001b[91m",
+    BrightGreen = "\u001b[92m",
+    BrightYellow = "\u001b[93m",
+    BrightBlue = "\u001b[94m",
+    BrightMagenta = "\u001b[95m",
+    BrightCyan = "\u001b[96m",
+    BrightWhite = "\u001b[97m",
+    Invert = "\u001b[7m",
+    Reset = "\u001b[0m",
+}
+
+export const green = `${AnsiEscapeSequences.BrightGreen}%s${AnsiEscapeSequences.Reset}`;
+export const cyan = `${AnsiEscapeSequences.BrightCyan}%s${AnsiEscapeSequences.Reset}`;
+export const magenta = `${AnsiEscapeSequences.BrightMagenta}%s${AnsiEscapeSequences.Reset}`;
+export const yellow = `${AnsiEscapeSequences.BrightYellow}%s${AnsiEscapeSequences.Reset}`;
+export const blue = `${AnsiEscapeSequences.BrightBlue}%s${AnsiEscapeSequences.Reset}`;
+export const invert = `${AnsiEscapeSequences.Invert}%s${AnsiEscapeSequences.Reset}`;
