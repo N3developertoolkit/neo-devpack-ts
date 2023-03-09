f4619a06c15b87bec6d50f65c7ecd99eba2d56ba
Harry Pierson <harrypierson@outlook.com>
2023-03-08 22:45:40 -0800

    fix contract

--- sample-contracts/helloworld.ts
+++ sample-contracts/helloworld.ts
@@ -10,29 +10,29 @@ const prefixContractOwner = 0xFF;
 
 /** @safe */
 export function get() { 
-    const key = Uint8Array.from([prefixSampleValue])
+    const key = ByteString.from([prefixSampleValue])
     return Storage.context.get(key);
 }
 
 export function set(value: ByteString) {
-    const key = Uint8Array.from([prefixSampleValue])
+    const key = ByteString.from([prefixSampleValue])
     Storage.context.put(key, value);
 }
 
 export function remove() {
-    const key = Uint8Array.from([prefixSampleValue])
+    const key = ByteString.from([prefixSampleValue])
     Storage.context.delete(key);
 }
 
 export function _deploy(_data: any, update: boolean): void { 
     if (update) return;
-    const key = Uint8Array.from([prefixContractOwner])
+    const key = ByteString.from([prefixContractOwner])
     const tx = Runtime.scriptContainer as Transaction;
     Storage.context.put(key, tx.sender);
 }
 
 export function update(nefFile: ByteString, manifest: string) {
-    const key = Uint8Array.from([prefixContractOwner])
+    const key = ByteString.from([prefixContractOwner])
     const owner = Storage.context.get(key)!;
     // TODO: support "if (owner && runtimeCheckWitness(owner))"
     if (checkWitness(owner)) {
