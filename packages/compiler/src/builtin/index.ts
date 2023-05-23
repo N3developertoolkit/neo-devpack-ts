a5c1861eb005635887f48af93e732d05867dab2e
Harry Pierson <harrypierson@hotmail.com>
2023-05-23 14:40:01 -0700

    metadata driven built ins

--- packages/compiler/src/builtin/index.ts
+++ packages/compiler/src/builtin/index.ts
@@ -9,40 +9,13 @@ import * as TS from "../TS";
 
 import { CompileTimeObject, Scope, createEmptyScope, createScope } from "../types/CompileTimeObject";
 import { LibraryDeclaration } from "../types/LibraryDeclaration";
-import { GlobalScopeContext } from "./types";
+import { GlobalScopeContext, parseSymbol, parseTypeSymbol } from "./types";
 import { CompileError, ParseError, createDiagnostic, makeParseDiagnostic } from "../utils";
 import { parseEnumDecl } from "../passes/parseDeclarations";
+import { Operation, parseOperation } from "../types/Operation";
+import { create } from "domain";
 
-class $GlobalScopeContext implements GlobalScopeContext {
-    readonly declMap: ReadonlyMap<string, readonly LibraryDeclaration[]>;
-    readonly errors: tsm.ts.Diagnostic[] = [];
-    readonly objects: CompileTimeObject[] = [];
-    readonly types: CompileTimeObject[] = [];
 
-    constructor(readonly decls: readonly LibraryDeclaration[]) {
-        const declMap = new Map<string, readonly LibraryDeclaration[]>();
-        for (const d of decls) {
-            const name = d.getName();
-            if (!name) throw new CompileError("invalid name", d);
-            const list = declMap.get(name) ?? [];
-            declMap.set(name, ROA.append(d)(list));
-        }
-        this.declMap = declMap;
-    }
-
-    addObject(obj: CompileTimeObject): void {
-        this.objects.push(obj);
-    }
-
-    addType(obj: CompileTimeObject): void {
-        this.types.push(obj);
-    }
-
-    addError(error: tsm.ts.Diagnostic | ParseError): void {
-        const diag = "message" in error ? makeParseDiagnostic(error) : error;
-        this.errors.push(diag);
-    }
-}
 
 function makeEnumObjects(ctx: GlobalScopeContext): void {
     // std TS lib does not define any enums
@@ -57,6 +30,145 @@ function makeEnumObjects(ctx: GlobalScopeContext): void {
     errors.forEach(ctx.addError);
     objects.forEach(ctx.addObject);
 }
+
+function makeNativeContracts(ctx: GlobalScopeContext) {
+    {
+        const { left: errors, right: objects } = pipe(
+            ctx.decls,
+            // find all the variable declarations that have the @nativeContract tag
+            ROA.filterMap(O.fromPredicate(tsm.Node.isVariableDeclaration)),
+            ROA.filter($var => pipe(
+                $var.getVariableStatement(),
+                O.fromNullable,
+                O.map(TS.hasTag("nativeContract")),
+                O.getOrElse(() => false)
+            )),
+            // create a CTO object for each @nativeContract variable 
+            ROA.map(node => pipe(
+                node,
+                parseSymbol,
+                // TODO: real CTO
+                E.map(symbol => <CompileTimeObject>{ node, symbol }),
+            )),
+            ROA.separate
+        );
+        errors.forEach(ctx.addError);
+        objects.forEach(ctx.addObject);
+    }
+
+    {
+        const { left: errors, right: types } = pipe(
+            ctx.decls,
+            ROA.filterMap(O.fromPredicate(tsm.Node.isInterfaceDeclaration)),
+            ROA.filter(TS.hasTag("nativeContract")),
+            ROA.map(node => pipe(
+                node,
+                parseTypeSymbol,
+                // TODO: real CTO
+                E.map(symbol => <CompileTimeObject>{ node, symbol }),
+            )),
+            ROA.separate
+        );
+        errors.forEach(ctx.addError);
+        types.forEach(ctx.addType);
+    }
+}
+
+const regexOperationTagComment = /(\S+)\s?(\S+)?/
+function makeOperationFunctions(ctx: GlobalScopeContext) {
+    const { left: errors, right: objects } = pipe(
+        ctx.decls,
+        // find all the function declarations that have the @syscall tag
+        ROA.filterMap(O.fromPredicate(tsm.Node.isFunctionDeclaration)),
+        ROA.filter(TS.hasTag("operation")),
+        ROA.map(makeFunction),
+        ROA.separate
+    );
+    errors.forEach(ctx.addError);
+    objects.forEach(ctx.addObject);
+
+    function makeFunction(node: tsm.FunctionDeclaration): E.Either<tsm.ts.Diagnostic, CompileTimeObject> {
+        return pipe(
+            E.Do,
+            E.bind("symbol", () => pipe(node, parseSymbol)),
+            // parse the @operations tags into an array of operations
+            E.bind("operations", () => pipe(
+                node.getJsDocs(),
+                ROA.chain(doc => doc.getTags()),
+                ROA.filter(tag => tag.getTagName() === 'operation'),
+                ROA.map(tag => tag.getCommentText() ?? ""),
+                ROA.map(parseOperationTagComment),
+                ROA.sequence(E.Applicative),
+                E.mapLeft(msg => createDiagnostic(msg, { node }))
+            )),
+            // TODO: real CTO
+            E.map(({ symbol, operations }) => <CompileTimeObject>{ node, symbol }),
+        );
+    }
+
+    function parseOperationTagComment(comment: string): E.Either<string, Operation> {
+        const matches = comment.match(regexOperationTagComment) ?? [];
+        return matches.length === 3
+            ? pipe(
+                parseOperation(matches[1], matches[2]),
+                E.fromNullable(comment)
+            )
+            : E.left(comment);
+    }
+
+}
+
+function makeSyscallFunctions(ctx: GlobalScopeContext) {
+    const { left: errors, right: objects } = pipe(
+        ctx.decls,
+        // find all the function declarations that have the @syscall tag
+        ROA.filterMap(O.fromPredicate(tsm.Node.isFunctionDeclaration)),
+        ROA.filter(TS.hasTag("syscall")),
+        ROA.map(makeFunction),
+        ROA.separate
+    );
+    errors.forEach(ctx.addError);
+    objects.forEach(ctx.addObject);
+
+    function makeFunction(node: tsm.FunctionDeclaration): E.Either<tsm.ts.Diagnostic, CompileTimeObject> {
+        return pipe(
+            E.Do,
+            E.bind("symbol", () => pipe(node, parseSymbol)),
+            E.bind("serviceName", () => pipe(
+                node,
+                TS.getTagComment('syscall'),
+                E.fromOption(() => createDiagnostic(`Invalid @syscall tag for ${node.getName()}`, { node }),
+                ))),
+            // TODO: real CTO
+            E.map(({ symbol, serviceName }) => <CompileTimeObject>{ node, symbol }),
+        );
+    }
+}
+
+function makeStackItemTypes(ctx: GlobalScopeContext) {
+    const { left: errors, right: types } = pipe(
+        ctx.decls,
+        ROA.filterMap(O.fromPredicate(tsm.Node.isInterfaceDeclaration)),
+        ROA.filter(TS.hasTag("stackitem")),
+        ROA.map(makeStackItemType),
+        ROA.separate
+    )
+    errors.forEach(ctx.addError);
+    types.forEach(ctx.addType);
+
+    function makeStackItemType(node: tsm.InterfaceDeclaration) {
+        return pipe(
+            E.Do,
+            E.bind("symbol", () => pipe(node, parseTypeSymbol)),
+            // TODO: real CTO
+            E.map(({ symbol }) => <CompileTimeObject>{ node, symbol }),
+        );
+    }
+}
+
+
+
+
 
 export function makeGlobalScope2(decls: readonly LibraryDeclaration[]): S.State<readonly tsm.ts.Diagnostic[], Scope> {
     return diagnostics => {
@@ -80,15 +192,20 @@ export function makeGlobalScope2(decls: readonly LibraryDeclaration[]): S.State<
             return [createEmptyScope(), ROA.concat(errors)(diagnostics)];
         }
 
-        const context: GlobalScopeContext = { 
-            decls, 
-            declMap, 
-            addError: (error: tsm.ts.Diagnostic) => { errors.push(error); }, 
-            addObject: (obj: CompileTimeObject) => { objects.push(obj); }, 
-            addType : (obj: CompileTimeObject) => { types.push(obj); }
+        const context: GlobalScopeContext = {
+            decls,
+            declMap,
+            addError: (error: tsm.ts.Diagnostic) => { errors.push(error); },
+            addObject: (obj: CompileTimeObject) => { objects.push(obj); },
+            addType: (obj: CompileTimeObject) => { types.push(obj); }
         }
 
+        // make all the metadata-driven built ins
         makeEnumObjects(context);
+        makeNativeContracts(context);
+        makeSyscallFunctions(context);
+        makeOperationFunctions(context);
+        makeStackItemTypes(context);
 
         diagnostics = ROA.concat(errors)(diagnostics);
         return pipe(
--- packages/compiler/src/builtin/types.ts
+++ packages/compiler/src/builtin/types.ts
@@ -1,8 +1,12 @@
 import * as tsm from "ts-morph";
 
+import { pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/Either";
+import * as TS from "../TS";
+
 import { CompileTimeObject } from "../types/CompileTimeObject";
 import { LibraryDeclaration } from "../types/LibraryDeclaration";
-import { ParseError } from "../utils";
+import { createDiagnostic } from "../utils";
 
 export interface GlobalScopeContext {
     readonly decls: readonly LibraryDeclaration[]
@@ -11,4 +15,20 @@ export interface GlobalScopeContext {
     addObject(obj: CompileTimeObject): void;
     addType(obj: CompileTimeObject): void;
     addError(error: tsm.ts.Diagnostic): void;
+}
+
+export function parseTypeSymbol(node: LibraryDeclaration) {
+    return pipe(
+        node,
+        TS.getSymbol,
+        E.fromOption(() => createDiagnostic(`could not get ${node.getName()} symbol`, { node }))
+    );
+}
+
+export function parseSymbol(node: LibraryDeclaration) {
+    return pipe(
+        node.getType(),
+        TS.getTypeSymbol,
+        E.fromOption(() => createDiagnostic(`could not get ${node.getName()} type symbol`, { node }))
+    );
 }
